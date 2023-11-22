cccb1d33b44666e89fd97324050c7cc2fd33ceaf
anjanachandramohan <anjanamohan.mc@gmail.com>
2023-11-22 15:03:34 +0800

    finalversion

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+import { useSelector } from 'react-redux';
+
+
+function Header() {
+  //to access the state value
+  const wishlist = useSelector((state)=>state.wishlistReducer)
+  /*console.log(wishlist);*/
+  const cart = useSelector((state)=>state.cartReducer)
+  console.log(cart);
+
+  
+  return (
+    <Navbar expand="lg" className="bg-primary fixed-top">
+    <Container>
+      <Navbar.Brand><Link style={{textDecoration:'none',color:'white'}} to={'/'}> <i class="fa-solid fa-cart-shopping fa-bounce me-3"></i> E-CART</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link className='btn border border-secondary rounded ms-3 mt-2'><Link to={'/wishlist'} 
+          style={{textDecoration:'none',color:'white'}}> <i class="fa-solid fa-heart me-2 text-danger">
+            </i>Wishlist<Badge bg="secondary" className='rounded ms-2' >{wishlist.length}</Badge></Link></Nav.Link>
+          <Nav.Link className='btn border border-secondary rounded ms-3 mt-2'><Link to={'/cart'} style={{textDecoration:'none',color:'white'}}>
+          <i class="fa-solid fa-cart-shopping me-2 text-warning"></i>Cart<Badge bg="secondary" className='rounded ms-2'>{cart.length}</Badge></Link></Nav.Link>
+         
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Header
